df3563fdd8f518c8e90f343feaa1545613fd3a1c
gib <gibbowden@gmail.com>
2018-03-15 10:19:11 -0500

    revenue report compolete - less formatting tweaks

---
+++ BladeRunnersBangazonCLI/Database/DataAccess/Models/CompletedOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace BladeRunnersBangazonCLI.Database.DataAccess.Models
+{
+    public class CompletedOrder : Order
+    {
+        public int Quantity { get; set; }
+        public string ProductName { get; set; }
+        public double ProductTotal { get; set; }
+
+    }
+}
---
+++ BladeRunnersBangazonCLI/Database/DataAccess/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace BladeRunnersBangazonCLI.Database.DataAccess.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int PaymentId { get; set; }
+
+    }
+}
